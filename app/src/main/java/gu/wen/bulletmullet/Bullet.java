222623d7d48eff8b10db78da3c4f66cc1b484c1d
Angel Wen <angelmwen@gmail.com>
2017-06-18 17:27:47 -0400

    modified Bullet, started db stuff

--- app/src/main/java/gu/wen/bulletmullet/Bullet.java
+++ app/src/main/java/gu/wen/bulletmullet/Bullet.java
@@ -1,30 +1,36 @@
 package gu.wen.bulletmullet;
 import java.util.*;
+import android.database.sqlite.*;
+
 /**
  * Created by angelwen on 6/18/17.
  */
 
 public class Bullet {
-    private String tableName = "bullets"; //or whatever you need to access bs
-    private String bulletType; //Task, Event, Note
-    private String text;
     private Date date;
 
-    public Bullet(String newBulletType, Date newDate, String newText){
-        //do stuff
-        bulletType = newBulletType;
+    private String dbName = "bulletJournalDB";
+    private String tableName = "bullets"; //or whatever you need to access bs
+    private static final String DATABASE_CREATE =
+            "create table productdet ("
+                    +"_id integer primary key autoincrement, "
+                    + "proname varchar ,"
+                    + "procost varchar);";
+    private SQLiteDatabase db;
+
+    public Bullet(Date newDate){
         date = newDate;
-        text = newText;
     }
 
-    private String getDbName(){
-        return "hi";
-    }
-    private void addBullet(){
+    /** add whatever bullet to the table in the database **/
+    public void addBullet(String bulletType, String text){//Task, Event, Note
 
     }
-    private Date getDate(){
-        Date date = new GregorianCalendar(2000,1 ,1).getTime();
+
+
+
+
+    public Date getDate(){
         return date;
     }
 
--- app/src/main/java/gu/wen/bulletmullet/DayEntry.java
+++ app/src/main/java/gu/wen/bulletmullet/DayEntry.java
@@ -6,13 +6,16 @@ import java.util.*;
 
 public class DayEntry {
     private Date date;
+    private Bullet bullet;
 
     public DayEntry(Date newDate){
         date = newDate;
+        bullet = new Bullet(newDate);
     }
 
+    /**called the addBullet method in Bullet**/
     public void addBullet(String bulletType, String text){
-
+        bullet.addBullet(bulletType, text);
     }
     public void reorderBullet(int prev_pos, int new_pos){
 
