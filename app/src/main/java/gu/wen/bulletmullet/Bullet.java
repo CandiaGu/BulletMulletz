8db22a7fd88fc8bbf3a7799c9f675fe485cefe19
Angel Wen <angelmwen@gmail.com>
2017-06-18 16:42:42 -0400

    added outline for Bullet Class

---
+++ app/src/main/java/gu/wen/bulletmullet/Bullet.java
@@ -0,0 +1,28 @@
+package gu.wen.bulletmullet;
+import java.util.*;
+/**
+ * Created by angelwen on 6/18/17.
+ */
+
+public class Bullet {
+    private String dbName = "bulletDb"; //or whatever you need to access bs
+    private String type; //Task, Event, Note
+    private String text;
+    private String bullet_type;
+
+    public Bullet(String bullet_type, Date date, String text){
+        //do stuff
+    }
+
+    private String getDbName(){
+        return "hi";
+    }
+    private void addBullet(){
+
+    }
+    private Date getDate(){
+        Date date = new GregorianCalendar(2000,1 ,1).getTime();
+        return date;
+    }
+
+}
