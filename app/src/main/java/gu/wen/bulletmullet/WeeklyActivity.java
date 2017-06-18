72ef86a75178ea306e8af57a3d65fd67b60cfb18
Angel Wen <angelmwen@gmail.com>
2017-06-18 17:07:04 -0400

    weekly base

--- app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
+++ app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
@@ -2,13 +2,49 @@ package gu.wen.bulletmullet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class WeeklyActivity extends AppCompatActivity {
-
+    private EditText edittext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly);
+        addKeyListener();
     }
+    public void addKeyListener() {
+        // get edittext component
+        edittext = (EditText) findViewById(R.id.editText);
 
+        // add a keylistener to keep track user input
+        edittext.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                // if keydown and "enter" is pressed
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                    // display a floating message
+                    Toast.makeText(WeeklyActivity.this,
+                            edittext.getText(), Toast.LENGTH_LONG).show();
+                    return true;
+
+                } else if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_9)) {
+
+                    // display a floating message
+                    Toast.makeText(WeeklyActivity.this,
+                            "Number 9 is pressed!", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
 }
+
