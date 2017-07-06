b014607a1193c9e158242eba951eb106b6098d45
CandiaGu <candi37@gmail.com>
2017-07-06 17:26:07 -0400

    added recyclerview

--- app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
+++ app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
@@ -2,20 +2,41 @@ package gu.wen.bulletmullet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnKeyListener;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.*;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WeeklyActivity extends AppCompatActivity {
     //private EditText edittext;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly);
        // addKeyListener();
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //temporary
+        String[] myDataset ={"1","2","3"};
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
+
+
     /*public void addKeyListener() {
         // get edittext component
         edittext = (EditText) findViewById(R.id.editText);
@@ -47,4 +68,7 @@ public class WeeklyActivity extends AppCompatActivity {
         });
     }*/
 }
+
+
+
 
