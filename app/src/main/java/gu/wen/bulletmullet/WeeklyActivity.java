d05e12729597ecc7a85caba69edfaa926fde7d19
Angel Wen <angelmwen@gmail.com>
2017-07-09 14:48:57 -0400

    yay i think

--- app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
+++ app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
@@ -1,20 +1,25 @@
 package gu.wen.bulletmullet;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import java.util.*;
 
 public class WeeklyActivity extends AppCompatActivity {
     private EditText edittext;
+    private static Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly);
         addKeyListener();
+        context = getApplicationContext();
     }
     public void addKeyListener() {
         // get edittext component
@@ -35,10 +40,19 @@ public class WeeklyActivity extends AppCompatActivity {
 
                 } else if ((event.getAction() == KeyEvent.ACTION_DOWN)
                         && (keyCode == KeyEvent.KEYCODE_9)) {
-
+                    DayEntry d = new DayEntry(context,Calendar.getInstance().getTime());
+                    d.addBullet("event","hello this is an event");
+                    d.addBullet("todo","todododododo");
+                    d.addBullet("task","hello do this task");
+                    LinkedList<String> events = d.getEventsList();
+                    Iterator itr = events.iterator();
+                    String events_str = "";
+                    while (itr.hasNext()){
+                        events_str = events_str +itr.next()+"\n";
+                    }
                     // display a floating message
                     Toast.makeText(WeeklyActivity.this,
-                            "Number 9 is pressed!", Toast.LENGTH_LONG).show();
+                            events_str, Toast.LENGTH_LONG).show();
                     return true;
                 }
 
