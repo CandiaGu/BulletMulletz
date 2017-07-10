d2acfa297381c5ce6c2719ba5ba3683fb051fbd8
CandiaGu <cagu@mbhs.edu>
2017-07-09 22:41:29 -0400

    added scrollview and initiateDayEntries

--- app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
+++ app/src/main/java/gu/wen/bulletmullet/WeeklyActivity.java
@@ -1,24 +1,38 @@
 package gu.wen.bulletmullet;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.*;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
 
 public class WeeklyActivity extends AppCompatActivity {
     //private EditText edittext;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    public DayEntry[] dayEntries;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly);
-       // addKeyListener();
+        setRecyclerView();
+
+        
+    }
+    public void setRecyclerView(){
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
@@ -29,44 +43,37 @@ public class WeeklyActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        //temporary
-        String[] myDataset ={"1","2","3"};
+        initiateDayEntries();
+
+        LinkedList<String> toDoList = dayEntries[0].getTodoList();
+        String[] myDataset =toDoList.toArray(new String[toDoList.size()]);
         // specify an adapter (see also next example)
         mAdapter = new MyAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+
+    }
+    //initiates the day entries for the current week, starting with monday of this week
+    public void initiateDayEntries(){
+        // get today and clear time of day
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+        cal.clear(Calendar.MINUTE);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MILLISECOND);
+        // get start of this week in milliseconds
+        cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+        for(int i = 0; i < 7; i++){
+            cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek()+i);
+            dayEntries[i] = new DayEntry(cal.getTime());
+        }
+
+    }
+    public void addTask(View view){
+        View parent = (View) view.getParent();
+
+
     }
 
-
-    /*public void addKeyListener() {
-        // get edittext component
-        edittext = (EditText) findViewById(R.id.editText);
-
-        // add a keylistener to keep track user input
-        edittext.setOnKeyListener(new OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                // if keydown and "enter" is pressed
-                if ((event.getAction() == KeyEvent.ACTION_DOWN)
-                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-
-                    // display a floating message
-                    Toast.makeText(WeeklyActivity.this,
-                            edittext.getText(), Toast.LENGTH_LONG).show();
-                    return true;
-
-                } else if ((event.getAction() == KeyEvent.ACTION_DOWN)
-                        && (keyCode == KeyEvent.KEYCODE_9)) {
-
-                    // display a floating message
-                    Toast.makeText(WeeklyActivity.this,
-                            "Number 9 is pressed!", Toast.LENGTH_LONG).show();
-                    return true;
-                }
-
-                return false;
-            }
-        });
-    }*/
 }
 
 
